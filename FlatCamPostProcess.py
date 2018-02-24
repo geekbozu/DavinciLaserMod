f6914446693fe6b44424244e7275d1a3b42f9938
geekboy1011 <geekboy1011@gmail.com>
2018-02-23 22:29:37 -0500

    Adjustments to Docs.txt Commented out debug code in Gcode mangler

--- FlatCamPostProcess.py
+++ FlatCamPostProcess.py
@@ -2,16 +2,17 @@ import re
 import fileinput
 p = re.compile('\A([GM])0(.)(.*)')
 
-for line in fileinput.input(inplace=0,backup='.bak'):
+for line in fileinput.input(inplace=1,backup='.bak'):
     t = p.match(line)
     if t != None:
         foo = ''
         for i in t.groups():
             i = re.sub('([XYZ])', r' \1', i)
             foo += i
-        
+        ''' Debugging shows filment output in repetier
         if t.group(2) == '1':
-            foo += ' E2'
+            foo += ' E2' 
+        '''
         print(foo)
     else:
         print line.strip()
